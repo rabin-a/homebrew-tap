95e0c0728a84e1cf3cb55ff402fcdde2169c9487
Rabin <rbnacharya@gmail.com>
2026-02-19 22:53:34 +0100

    Fix checksums for v0.1.0 release

--- Formula/intentflow.rb
+++ Formula/intentflow.rb
@@ -7,10 +7,10 @@ class Intentflow < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/rabin-a/intentflow/releases/download/v#{version}/intentflow-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "82df58a58ee069c3a13bda761e1e6b1eaef27b38231294e689a8e945b856d102"
     else
       url "https://github.com/rabin-a/intentflow/releases/download/v#{version}/intentflow-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "14e4e899c84128f1ca49c4f96a74660698d4f797f8ba02784c4fd6ffd322ceba"
     end
   end
 
